035e9b0c708329cd810aafcc0a16f6dd0106861f
mrfarang <paxzagrom@gmail.com>
2026-04-26 15:37:03 +0200

    init

---
+++ src/main/kotlin/com/alfred/serv/drinkmonitor/DrinkmonitorApplication.kt
@@ -0,0 +1,11 @@
+package com.alfred.serv.drinkmonitor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DrinkmonitorApplication
+
+fun main(args: Array<String>) {
+	runApplication<DrinkmonitorApplication>(*args)
+}
---
+++ src/test/kotlin/com/alfred/serv/drinkmonitor/DrinkmonitorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alfred.serv.drinkmonitor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DrinkmonitorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
